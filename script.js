3affdfe0e717f32b057e6e2007e1cf87a3958f3e
Author Name <stelmakh.natallia@gmail.com>
2020-11-01 01:24:40 -0400

    fixing small bugs -parentesis in place now

--- script.js
+++ script.js
@@ -52,32 +52,34 @@ class Calculator {
   }
 
   getDisplayNumber(number) {
-    const stringNumber = number.toString()
-    const integerDigits = parseFloat(stringNumber.split(".")[0])
-    const decimalDigits = stringNumber.split(".")[1]
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
 
-let integerDisplay
-if (isNaN(integerDigits)){
-  integerDisplay =""
-}else{
-  integerDisplay = integerDigits.toLocaleString("en", {
-    maximumFractionDigits:0})
-}
-if (decimalDigits!=null){
-  return `${integerDisplay}.${decimalDigits}`
-} else{
-  return integerDisplay
-}
-
-}
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
   }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
     if (this.operation != null) {
       this.previousOperandTextElement = `${this.previousOperand} ${this.operation}`;
     } else {
-    this.previousOperandTextElement.innerText = "";
-  
+      this.previousOperandTextElement.innerText = "";
+    }
   }
 }
 
